2942ee74f55a08bf9ef1d94b6e9a846b8a4a52f1
Aashra Pradhan <programmeraashra@gmail.com>
2024-06-15 22:26:51 +0545

    aashra talks

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,28 +1,41 @@
 //getting all contacts
-export const getContacts = (req, res) => {
-  res.status(200).send("Hello World!");
+export const getContacts = async (req, res) => {
+  try {
+    res.status(200).json({ message: "Fetched all contacts succesfully!" });
+  } catch {}
 };
 
 // getting 1 contact
-export const getContact = (req, res) => {
+export const getContact = async (req, res) => {
   try {
+    res
+      .status(200)
+      .json({ message: `Fetched contact ${req.params.id} succesfully!` });
+    // The req.params object in Express.js holds all the parameters that are included in the URL path. For instance, if you have a route defined as /user/:id, and you make a request to /user/123, then req.params.id will hold the value 123.
   } catch {}
 };
 
 // creating a contact
-export const createContact = (req, res) => {
+export const createContact = async (req, res) => {
   try {
+    res.status(201).json({ message: "Created contact succesfully!" });
   } catch {}
 };
 
 // updating a contact
-export const updateContact = (req, res) => {
+export const updateContact = async (req, res) => {
   try {
+    res
+      .status(200)
+      .json({ message: `Updated contact ${req.params.id} succesfully!` });
   } catch {}
 };
 
 // deleting a contact
-export const deleteContact = (req, res) => {
+export const deleteContact = async (req, res) => {
   try {
+    res
+      .status(200)
+      .json({ message: `Contact ${req.params.id} deleted  succesfully!` });
   } catch {}
 };
